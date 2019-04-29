2f6eb9038c9319cb0257dce5e525bf9b14fdf697
Paul Varache <paul@kano.me>
2019-04-29 10:35:42 +0100

    Update dep to resolution, add replacements option

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const serveStatic = require('serve-static');
 
 const namedResolutionMiddleware = require('./named-resolution-middleware');
 
-module.exports = ({ root = process.cwd(), port = 4000 } = {}) => {
+module.exports = ({ root = process.cwd(), port = 4000, replacements = [] } = {}) => {
     return connect()
-        .use(namedResolutionMiddleware({ root }))
+        .use(namedResolutionMiddleware({ root, replacements }))
         .use(serveStatic(root))
         .listen(port);
 }
--- named-resolution-middleware.js
+++ named-resolution-middleware.js
@@ -7,7 +7,7 @@ const textChunk = require('node-text-chunk');
 
 module.exports = (opts = {}) => {
     const root = opts.root || process.cwd();
-    const onModule = opts.onModule || function () {};
+    const replacements = opts.replacements || [];
 
     return (req, res, next) => {
         const _end = res.end;
@@ -41,7 +41,7 @@ module.exports = (opts = {}) => {
         }
 
         function transformAndSend() {
-            upgradedBody = resolveNamedPath(root, buffer, contentType, filePath, req.originalUrl, onModule);
+            upgradedBody = resolveNamedPath(root, buffer, contentType, filePath, replacements);
             queue = textChunk.text(upgradedBody, 1024);
             res.setHeader('Content-Length', Buffer.byteLength(upgradedBody, 'utf-8'));
             if (status) {
